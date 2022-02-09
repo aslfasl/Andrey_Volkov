fd193d45895221fe19a4a158ca7334a1d73c97c2
VolkovAndrey <soupec@yandex.ru>
2022-02-09 16:32:16 +0300

    @Value minAge/maxAge

--- src/main/java/com/Volkov/service/ValidationService.java
+++ src/main/java/com/Volkov/service/ValidationService.java
@@ -4,11 +4,16 @@ import com.Volkov.db.entity.CarEntity;
 import com.Volkov.exceptions.ErrorType;
 import com.Volkov.exceptions.InsuranceException;
 import com.Volkov.exceptions.WrongAgeException;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 
 public class ValidationService {
+    @Value("${custom.validation.minAge}")
+    static int minAge;
+    @Value("${custom.validation.maxAge}")
+    static int maxAge;
 
     public static boolean insuranceCheck(CarEntity car) throws InsuranceException {
         if (car.isInsurance()) {
@@ -24,7 +29,7 @@ public class ValidationService {
             throw new WrongAgeException("This age is not allowed");
         } else {
             int age = (int) ChronoUnit.YEARS.between(birthDate, today);
-            if (age <= 18 || age >= 65) {
+            if (age <= minAge || age >= maxAge) {
                 throw new WrongAgeException("This age is not allowed");
             }
         }
