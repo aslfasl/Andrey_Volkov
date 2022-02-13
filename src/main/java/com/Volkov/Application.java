6b6579b4fbf1088c6f93b02dca3d314d53f34392
VolkovAndrey <soupec@yandex.ru>
2022-02-13 11:25:09 +0300

    Swagger added

--- src/main/java/com/Volkov/Application.java
+++ src/main/java/com/Volkov/Application.java
@@ -6,10 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableScheduling
+@EnableSwagger2
 public class Application {
 
 	public static void main(String[] args) {
--- src/main/java/com/Volkov/service/WeatherGetter.java
+++ src/main/java/com/Volkov/service/WeatherGetter.java
@@ -1,29 +1,29 @@
-//package com.Volkov.service;
-//
-//import com.Volkov.web.OtherClient;
-//import lombok.RequiredArgsConstructor;
-//import lombok.SneakyThrows;
-//import org.springframework.scheduling.annotation.Scheduled;
-//import org.springframework.stereotype.Service;
-//
-//import javax.annotation.PostConstruct;
-//
-//@Service
-//@RequiredArgsConstructor
-//public class WeatherGetter {
-//
-//    private final OtherClient otherClient;
-//
-//    @Scheduled(initialDelay = 7000L, fixedRate = 14000L)
-//    public void clicker() {
-//        String weather = otherClient.getWeather();
-//        String forecast = weather.substring(weather.indexOf("forecast")-1, weather.indexOf("debug")) ;
-//        System.out.println(forecast);
-//    }
-//
-//    @SneakyThrows
-//    @PostConstruct
-//    public void init() {
-//        clicker();
-//    }
-//}
+package com.Volkov.service;
+
+import com.Volkov.web.OtherClient;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+
+@Service
+@RequiredArgsConstructor
+public class WeatherGetter {
+
+    private final OtherClient otherClient;
+
+    @Scheduled(initialDelay = 7000L, fixedRate = 14000L)
+    public void clicker() {
+        String weather = otherClient.getWeather();
+        String forecast = weather.substring(weather.indexOf("forecast")-1, weather.indexOf("debug")) ;
+        System.out.println(forecast);
+    }
+
+    @SneakyThrows
+    @PostConstruct
+    public void init() {
+        clicker();
+    }
+}
