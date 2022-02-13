61a562489875f507ff72ca87fc22aacbcd35ca4b
VolkovAndrey <soupec@yandex.ru>
2022-02-13 17:58:41 +0300

    Swagger config added

--- src/main/java/com/Volkov/Application.java
+++ src/main/java/com/Volkov/Application.java
@@ -5,7 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -18,5 +23,15 @@ public class Application {
 		ConfigurableApplicationContext applicationContext = SpringApplication.run(Application.class, args);
 		WhatTime whatTime = applicationContext.getBean(WhatTime.class);
 		whatTime.isNow();
+	}
+
+	@Bean
+	public Docket swaggerConfig() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select() // API selector builder
+//				.paths(PathSelectors.ant("/cars/*"))
+				.paths(PathSelectors.ant("/drivers/*"))
+				.apis(RequestHandlerSelectors.basePackage("com.Volkov"))
+				.build();
 	}
 }
